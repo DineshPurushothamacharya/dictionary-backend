168baaffcd8782f97f549bd6b09b6ce303d8dd8c
Dinesh Purushothamacharya <dinesh.purushotham@picsart.com>
2024-01-10 15:51:05 +0100

    initial commit

---
+++ src/main/kotlin/com/dinesh/dictionarybackend/DictionaryBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.dinesh.dictionarybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DictionaryBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DictionaryBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/dinesh/dictionarybackend/DictionaryBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dinesh.dictionarybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DictionaryBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
